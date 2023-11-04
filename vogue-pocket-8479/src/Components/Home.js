9ebb783e60e3c6f1e02d961302636a287a1ba3ab
Piyush Sharma <118152296+pspiyush130@users.noreply.github.com>
2023-11-04 22:27:41 +0530

    homepage

---
+++ vogue-pocket-8479/src/Components/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import BannerBackground from "../Assets/home-banner-background.png";
+import BannerImage from "../Assets/home-banner-image.png";
+import Navbar from "./Navbar";
+import { FiArrowRight } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+           Your health is our responsibility
+          </h1>
+          <p className="primary-text">
+          Say Hello to India's top doctors via video consultation, get digital prescriptions, order medicines, book doctor appointments & lab tests, find doctors near 
+          </p>
+          <button className="secondary-button">
+            Book Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
